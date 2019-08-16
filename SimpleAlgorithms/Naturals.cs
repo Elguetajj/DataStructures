4bb14870e233ae42434153657651f02558ec8f38
Juanjo <elguetaj@ufm.edu>
2019-08-16 13:16:25 -0600

    Organized some files

---
+++ SimpleAlgorithms/Naturals.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace SimpleAlgorithms
+{
+    public class Naturals
+    {
+        public static int sumNaturals(int n)
+        //Algebraic way of finding the sum of the first n natural numbers
+        {
+            if (n > 0)
+            {
+                return n * (n + 1) / 2;
+            }
+            else throw new ArgumentOutOfRangeException("n", "Number must be a natural number");
+        }
+    }
+}
---
+++ SimpleAlgorithms/Rotate.cs
@@ -0,0 +1,23 @@
+﻿namespace SimpleAlgorithms
+{
+    public class Rotate
+    {
+        public static void Reverse(int[] arr, int first, int last)
+        {
+            int temp;
+            for (int i = first, j = last; i < j; i++, j--)
+            {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        public static void RotateN(int[] arr, int n)
+        {
+            Reverse(arr, arr.Length - n, arr.Length - 1);
+            Reverse(arr, 0, arr.Length - 1 - n);
+            Reverse(arr, 0, arr.Length - 1);
+        }
+    }
+}
